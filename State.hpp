3ade453d5c59a4806825008eae87d7a72780adbe
RoelStierum <roel.stierum@gmail.com>
2021-01-27 15:03:24 +0100

    added doxygen and comments

--- State.hpp
+++ State.hpp
@@ -4,12 +4,17 @@ namespace engine{
 
     class State{
     public:
+        ///@brief Virtual void initialisation function
         virtual void Init()=0;
+        ///@brief Virtual void HandleInput function
         virtual void HandleInput()=0;
+        ///@brief Virtual void Update function
         virtual void Update(float dt)=0;
+        ///@brief Virtual void Draw function
         virtual void Draw(float dt)=0;
-
+        ///@brief Virtual void Pause function
         virtual void Pause(){}
+        ///@brief Virtual void Resume function
         virtual void Resume(){}
     };
 
--- controlState.hpp
+++ controlState.hpp
@@ -6,15 +6,23 @@
 #include "Game.hpp"
 
 namespace engine{
-
+    ///@brief ControlState Class
+    ///@details Class to display the instructions to play the game
     class ControlState: public State{
     public:
+
         ControlState(GameDataRef data);
-
+        ///@brief Initialisation function.
+        ///@details Initialisation function for the loading the data assets from the images.
         void Init();
-
+        ///@brief Input function for mouse.
+        ///@details Function which interacts with the buttons, for instance the highlighting and clicking.
         void HandleInput();
+        ///@brief Update function.
+        ///@details Function for using the esc key to exit the instructions screen.
         void Update(float dt);
+        ///@brief Draw function.
+        ///@details Function for drawing the images on the screen
         void Draw(float dt);
 
     private:
